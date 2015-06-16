c1bd37686d59013a7c35dd41184ddecdfed56981
iraritchiemeek <iraritchiemeek@gmail.com>
2015-06-17 11:10:28 +1200

    Add getorientaion and getsize

--- controller.js
+++ controller.js
@@ -2,5 +2,11 @@ $(window).load(function(){
 
 	var rectangle = new Rectangle()
 
-	rectangle.initialRect()
+	rectangle.appendInitialRect()
+
+	$(document).on("mousemove", ".rect", function(e){
+		var size = rectangle.getSize(e.target)
+		var orientation = rectangle.getOrientation(size)
+		rectangle.splitDiv(e.target, size, orientation)
+	})
 })
--- rectangles.js
+++ rectangles.js
@@ -1,10 +1,26 @@
 function Rectangle () {
 	this.wHeight = window.innerHeight;
 	this.wWidth = window.innerWidth;
-	
 }
 
-Rectangle.prototype.initialRect = function() {
+Rectangle.prototype.appendInitialRect = function() {
 	$("#container").append("<div class='rect'/>")
 	$(".rect").css({height:this.wHeight, width:this.wWidth, backgroundColor:getRandomColor()})
+};
+
+Rectangle.prototype.getSize = function(rectangle) {
+	return [$(rectangle).width(), $(rectangle).height()]
+};
+
+Rectangle.prototype.getOrientation = function(size) {
+	if (size[0] < size[1]) {
+		this.orientation = "landscape"
+	} else {
+		this.orientation = "portrait"
+	}
+	return this.orientation
+};
+
+Rectangle.prototype.splitDiv = function(rectangle, size, orientation) {
+	console.log(orientation)
 };
